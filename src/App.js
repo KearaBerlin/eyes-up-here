1eed577bbf0428567be9384b36d83caf541c52c6
Keara Berlin <kearaberlin@gmail.com>
2021-03-09 19:46:57 -0600

    detect one face, but it throws an error about emotion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
@@ -9,7 +8,59 @@ class App extends React.Component {
     // constants for Azure quickstart
     const msRest = require("@azure/ms-rest-js");
     const Face = require("@azure/cognitiveservices-face");
-    const uuid = require("uuid/v4");
+
+    const key = "7e9612f79e204cd2b45ca8b5394acdd7";
+    const endpoint = "https://eyesupherefaceinstance.cognitiveservices.azure.com/";
+
+    // authenticate the client for azure quickstart
+    const credentials = new msRest.ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': key } });
+    const client = new Face.FaceClient(credentials, endpoint);
+
+    this.state = {
+      image_base_url: "https://csdx.blob.core.windows.net/resources/Face/Images/",
+      client: client
+    }
+
+    this.detectFace();
+  }
+
+  async detectFace() {
+    let image_file_name = "detection1.jpg";
+    let face = this.state.client.face;
+    const detectedFace = await this.state.client.face.detectWithUrl(
+        this.state.image_base_url + image_file_name,
+        {
+          returnFaceAttributes: ["Emotion", "HeadPose"],
+          detectionModel: "detection_01"
+        }
+      );
+    console.log (detectedFace.length + " face(s) detected from image " + image_file_name + ".");
+    console.log("Face attributes for face(s) in " + image_file_name + ":");
+
+    // Get emotion on the face
+    let emotions = "";
+    let emotion_threshold = 0.0;
+    if (face.faceAttributes.emotion.anger > emotion_threshold) { emotions += "anger, "; }
+    if (face.faceAttributes.emotion.contempt > emotion_threshold) { emotions += "contempt, "; }
+    if (face.faceAttributes.emotion.disgust > emotion_threshold) { emotions +=  "disgust, "; }
+    if (face.faceAttributes.emotion.fear > emotion_threshold) { emotions +=  "fear, "; }
+    if (face.faceAttributes.emotion.happiness > emotion_threshold) { emotions +=  "happiness, "; }
+    if (face.faceAttributes.emotion.neutral > emotion_threshold) { emotions +=  "neutral, "; }
+    if (face.faceAttributes.emotion.sadness > emotion_threshold) { emotions +=  "sadness, "; }
+    if (face.faceAttributes.emotion.surprise > emotion_threshold) { emotions +=  "surprise, "; }
+    if (emotions.length > 0) {
+        console.log ("Emotions: " + emotions.slice (0, -2));
+    }
+    else {
+        console.log ("No emotions detected.");
+    }
+
+    // get head pose
+    // Get more attributes
+    console.log("Head pose:");
+    console.log("  Pitch: " + face.faceAttributes.headPose.pitch);
+    console.log("  Roll: " + face.faceAttributes.headPose.roll);
+    console.log("  Yaw: " + face.faceAttributes.headPose.yaw);
   }
 
   render() {
